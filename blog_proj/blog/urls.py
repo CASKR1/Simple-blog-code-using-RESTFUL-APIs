cf91aebbf144fe43d056a5e59af5fad9ba68d30b
CASKR1 <128218098+CASKR1@users.noreply.github.com>
2023-03-31 18:39:27 +0530

    Delete blog_proj directory

--- blog_proj/blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
-from . import views
-
-urlpatterns = format_suffix_patterns([
-    path('api/', views.PostViewSet.as_view(actions={'get': 'list'})),
-])
